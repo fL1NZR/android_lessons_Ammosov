ea74a23d068b18740e352e1b8f33fc351d7aae72
Kira Doma <eeparengandon@icloud.com>
2023-02-13 14:52:46 +0900

    привет пока

---
+++ app/src/main/java/com/example/androidlessonsammosov/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidlessonsammosov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
